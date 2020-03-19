8c035e4ad43ba7932e8f2a01090c81af6904c1ae
KorayAydemir <korayaydemir276@gmail.com>
2020-03-19 16:02:02 +0300

    Simple Player State Machine Completed
    A state machine for running, jumping, idling, falling and their animations are added.

--- Worst Platformer Ever/Scenes/Player/Player.gd
+++ Worst Platformer Ever/Scenes/Player/Player.gd
@@ -25,6 +25,7 @@ var min_jump_height = 0.76 * Globals.UNIT_SIZE
 var jump_duration = 0.4
 var fall_duration = 0.35
 
+onready var drop_thru_raycasts = $DropThruRaycasts
 onready var raycasts = $Raycasts
 onready var anim_player = $Body/Sprite/AnimationPlayer
 
@@ -34,23 +35,24 @@ func _ready():
 	fall_gravity = 2 * max_jump_height / pow(fall_duration, 2)
 	max_jump_velocity = -sqrt(2 * gravity * max_jump_height)
 	min_jump_velocity = -sqrt (2 * gravity * min_jump_height)
-
-func _physics_process(delta):
-	_get_input() # moving horizontally
+	
+	
+func _apply_gravity(delta):
+	if is_jumping:
+		velocity.y += gravity * delta 
+	else: # fall gravity
+		velocity.y += fall_gravity * delta
+		
+func _apply_movement():
 	if velocity.y >= 0:
-		is_jumping = false
+		is_jumping = false    
 	else:
 		is_jumping = true
-		
 	if velocity.y > 0:
 		is_falling = true
 	else:
 		is_falling = false
-	# apply jump gravity and fall gravity
-	if is_jumping:
-		velocity.y += gravity * delta 
-	else:
-		velocity.y += fall_gravity * delta
+		
 		
 	velocity = move_and_slide(velocity, UP)
 	is_grounded = !is_jumping  && get_collision_mask_bit(DROP_THRU_BIT) && _check_is_grounded()
@@ -61,19 +63,7 @@ func _physics_process(delta):
 	if was_grounded == null || is_grounded != was_grounded:
 		emit_signal("grounded_updated", is_grounded)
 	
-func _input(event):
-	# if jump is just pressed and char is grounded then jump
-	if event.is_action_pressed("jump") && is_grounded:
-		velocity.y = max_jump_velocity
-	# if jump key is released, fall
-	if event.is_action_released("jump") && velocity.y < min_jump_velocity:
-		velocity.y = min_jump_velocity
-	# drop through platform
-	if Input.is_action_pressed("down"):
-		if _check_is_grounded($DropThruRaycasts):
-			set_collision_mask_bit(DROP_THRU_BIT, false)
-	
-func _get_input(): # hold inputs
+func _handle_move_input(): # hold inputs
 	 # horizontal movement direction
 	move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
 	 # lerp(from, to, smoothness to apply)	
---
+++ Worst Platformer Ever/Scenes/Player/PlayerFSM.gd
@@ -0,0 +1,80 @@
+extends "res://StateMachine.gd"
+
+func _ready():
+	add_state("idle")
+	add_state("run")
+	add_state("jump")
+	add_state("fall")
+	call_deferred("set_state", states.idle)
+
+
+func _input(event):
+	# check if our current state is idle or run
+	if [states.idle, states.run].has(state):
+		# if jump is just pressed and char is grounded then jump
+		if event.is_action_pressed("jump"):
+			parent.velocity.y = parent.max_jump_velocity
+		
+		# drop through platform
+		if Input.is_action_pressed("down"):
+			if parent._check_is_grounded(parent.drop_thru_raycasts): #parent.drop_thru_raycasts on video
+				parent.set_collision_mask_bit(parent.DROP_THRU_BIT, false)
+				
+	# check if our current state is jump
+	if state == states.jump:
+		# if jump key is released, fall
+		if event.is_action_released("jump") && parent.velocity.y < parent.min_jump_velocity:
+			parent.velocity.y = parent.min_jump_velocity
+
+func _state_logic(delta):
+	parent._handle_move_input()
+	parent._apply_gravity(delta)
+	parent._apply_movement()
+	
+func _get_transition(delta):
+	match state:
+		states.idle: # in idle satate
+			if !parent.is_on_floor(): # if not on the ground
+				if parent.is_jumping:  # if moving upwards
+					return states.jump # switch to jump state
+				elif parent.is_falling: # if moving downwards
+					return states.fall # switch to fall state
+			if parent.move_direction != 0: # if we are moving horizontally
+				return states.run # switch to run state
+		
+		states.run: # in idle satate
+			if !parent.is_on_floor(): # if not on the ground
+				if parent.is_jumping:  # if moving upwards
+					return states.jump # switch to jump state
+				elif parent.is_falling: # if moving downwards
+					return states.fall # switch to fall state
+			if parent.move_direction == 0: # if we are not moving horizontally
+				return states.idle # switch to idle state
+				
+		states.jump:
+			if  parent.is_on_floor(): # if on the ground
+				return states.idle	# switch to idle state
+			elif parent.is_falling: # if moving downwards
+				return states.fall # switch to fall state
+				
+		states.fall:
+			if  parent.is_on_floor(): # if on the ground
+				return states.idle	# switch to idle state
+			elif parent.is_jumping: # if moving upwards
+				return states.jump
+	return null
+	
+func _enter_state(new_state, old_state):
+	match new_state:
+		states.idle:
+			parent.anim_player.play("Idle")
+		states.run:
+			parent.anim_player.play("Run")
+		states.jump:
+			parent.anim_player.play("Jump")
+		states.fall:
+			parent.anim_player.play("Fall")
+	
+func _exit_state(old_state, new_state):
+	pass
+	
